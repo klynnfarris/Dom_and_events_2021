9a6cd15506e8c625f7467548865617497dbaf514
Katie Farris <klynnfarris@gmail.com>
2021-06-03 18:43:45 -0500

    first commit

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
 // Write your JavaScript code here.
-// Remember to pay attention to page loading!
+
+
+function init (){
+     let takeOffButton = document.getElementById("takeoff");
+     takeOffButton.addEventListener("click", confirmwindow);
+     let confirmwindow = confirm("Confirm that the shuttle is ready for takeoff.");
+}
+
+//window load event
+window.addEventListener("load", init);    
+
